31fb042bdc90fcf3a2bb4017d02eab6aad41af37
Shahzeb <shahzeb@Lahishs-MacBook-Air.local>
2023-09-15 19:51:37 +0500

    test

--- test/bubble_sort_example.py
+++ test/bubble_sort_example.py
@@ -1,14 +1,22 @@
 import pygame
 import numpy as np
-from random import randint
+from random import randint, shuffle
+from math import ceil
 
-def randomize():
+complete = False
+
+def randomize(num):
     global x
-    x = list(np.random.randint(low = 0,high=75,size=randint(5, 100)))
+    if num == 0:
+      x = list(np.random.randint(low = 5,high=70,size=randint(5, 100)))
+    else:
+      x = list(range(1,71))
+      shuffle(x)
     global width
     width = 700 / len(x)
-    width = width - width / 10
+    width = width - ceil(width / 10 / 10 + width / 10)
     print(len(x), width)
+    complete = False
 
 def swap(arr, j1, j2):
     temp = arr[j1]
@@ -25,25 +33,31 @@ def bubble(nums):
                 swapped = True
                 swap(nums, j, j + 1)
         if not swapped:
-            break        
-                
-                
+            complete = True
+            break
+    print(nums)
+               
+               
 def update(arr, green):
-    print(green)
     screen.fill("purple")
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-    y = 50
+    y = (800 - (width * len(arr) + ceil(width / 10 + width))) / 2
+    print(y)
     for i in range(len(arr)):
         block_surf = pygame.Surface((width,arr[i] * 10))
-        block_rect = block_surf.get_rect(midbottom = (y, 790))
-        y += int(width / 10 + width)
-        if not i in green:
-            block_surf.fill("White")
+        block_rect = block_surf.get_rect(midbottom = (y, 720))
+        y += ceil(width / 10 + width)
+        if green:
+          if i in green:
+              block_surf.fill("Green")
+        if complete:
+          block_surf.fill("Red")
         else:
-            block_surf.fill("Green")
+          block_surf.fill("White")
+           
         screen.blit(block_surf, block_rect)
     pygame.display.update()
     clock.tick(60)
@@ -56,13 +70,17 @@ def main():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_k:     
+                if event.key == pygame.K_k:    
                     bubble(x)
                 if event.key == pygame.K_l:
-                    randomize()
+                    randomize(0)
                     print(x)
+                if event.key == pygame.K_j:
+                    randomize(1)
+                    print(x)
+        update(x, [])
         pygame.display.update()
-        clock.tick(10)
+        clock.tick(1000)
 
-randomize()
+randomize(0)
 main()
